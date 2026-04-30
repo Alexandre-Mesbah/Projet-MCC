72be6a9f67e57e084fb726893efa43a8805a2105
Badis <161819322+uvsq22301033@users.noreply.github.com>
2026-04-30 20:47:29 +0200

    Refactor projet.py with improved readability
    Refactor methods and improve comments in projet.py

--- projet.py
+++ projet.py
@@ -1,6 +1,4 @@
-if __name__ == "__main__":
-
-    class Machine_turing :
+class Machine_turing :
         def __init__(self, alphabet, transitions, etat_initial, etat_acceptant, blanc, nb_rubans):
             self.alphabet = alphabet
             self.transitions = transitions
@@ -10,54 +8,54 @@ if __name__ == "__main__":
             self.nb_rubans = nb_rubans
 
 
-    class Configuration :
+class Configuration :
         def __init__(self, rubans, positions, etat):
             self.rubans = rubans
             self.positions = positions
             self.etat = etat
 
-    def un_pas(machine, config):
-        symboles_lus = []
-        for i in range(machine.nb_rubans):
-            position = config.positions[i]
-            ruban = config.rubans[i]
+def un_pas(machine, config):
+        symboles_lus = [] #iniatialisation de la liste des symboles lus, si la machine a plusieurs rubans, on lira un symbole sur chaque ruban et on les stockera dans cette liste
+        for i in range(machine.nb_rubans):  #pour chauque ruban
+            position = config.positions[i]  #position de la tete de lecture sur le ruban i
+            ruban = config.rubans[i] #pour le ruban i
 
-            if position < 0:
+            if position < 0:    #si la position est négative, on ajoute un blanc au début du ruban et on remet la position à 0
                 ruban.insert(0, machine.blanc)
                 config.positions[i] = 0
                 position = 0
 
-            if position >= len(ruban):
+            if position >= len(ruban):  #si la position dépasse la longueur du ruban, on ajoute un blanc à la fin du ruban
                 ruban.append(machine.blanc)
 
-            symboles_lus.append(ruban[position])
+            symboles_lus.append(ruban[position])    #on ajoute le symbole lu à la liste des symboles lus
 
-        if machine.nb_rubans == 1:
-            cle = (config.etat, symboles_lus[0])
+        if machine.nb_rubans == 1:      #si la machine a un seul ruban, on utilise une clé simple pour les transitions
+            cle = (config.etat, symboles_lus[0])    #la cle est un tuple composé de l'état courant, et du symbole que l'on lit
         else:
-            cle = (config.etat, tuple(symboles_lus))
+            cle = (config.etat, tuple(symboles_lus))    #si la machine a plusieurs rubans, on utilise une clé composée de l'état courant et d'un tuple des symboles lus sur chaque ruban
 
-        if cle not in machine.transitions:
+        if cle not in machine.transitions:  #si la clé n'est pas dans les transitions de la machine, cela signifie que la machine est bloquée et ne peut pas continuer, on retourne None pour indiquer ce blocage
             return None
 
-        nouvel_etat, symboles_ecrits, directions = machine.transitions[cle]
+        nouvel_etat, symboles_ecrits, directions = machine.transitions[cle]    #si la clé est dans les transitions, on récupère le nouvel état, les symboles à écrire et les directions à suivre pour chaque ruban
 
-        if machine.nb_rubans == 1:
-            symboles_ecrits = [symboles_ecrits]
+        if machine.nb_rubans == 1: 
+            symboles_ecrits = [symboles_ecrits] 
             directions = [directions]
 
-        for i in range(machine.nb_rubans):
-            position = config.positions[i]
-            ruban = config.rubans[i]
-            ruban[position] = symboles_ecrits[i]
+        for i in range(machine.nb_rubans):  #pour chaque ruban i,
+            position = config.positions[i]  #on récupère la position de la tête de lecture sur le ruban i
+            ruban = config.rubans[i]    #on récupère le ruban i
+            ruban[position] = symboles_ecrits[i]   #on écrit le symbole correspondant à ce ruban i à la position de la tête de lecture
 
         config.etat = nouvel_etat
 
-        for i in range(machine.nb_rubans):
-            direction = directions[i]
+        for i in range(machine.nb_rubans):  #pour chaque ruban i,
+            direction = directions[i]   #On recupere la direction à suivre pour le ruban i
 
-            if direction in ('R', '>'):
-                config.positions[i] += 1
+            if direction in ('R', '>'): #si la direction est à droite,
+                config.positions[i] += 1    #on incrémente la position de la tête de lecture sur le ruban i
             elif direction in ('L', '<'):
                 config.positions[i] -= 1
 
@@ -73,7 +71,15 @@ if __name__ == "__main__":
         return config
 
 
-    def lire_machine(chemin_fichier):
+def lire_machine(chemin_fichier):
+        """Selon le format de la machine dans le fichier texte, on lit les lignes du fichier et on construit la machine de Turing correspondante
+        le format est le suivant :
+        name: nom_de_la_machine (optionnel)
+        init: etat_initial
+        accept: etat_acceptant
+        etat_source, symbole_lu1, symbole_lu2, ...
+        etat_destination, symbole_ecrit1, symbole_ecrit2, ..., direction1, direction2, ...
+        """
         with open(chemin_fichier, 'r', encoding='utf-8') as f:
             lignes = [ligne.strip() for ligne in f if ligne.strip()]
 
@@ -88,22 +94,22 @@ if __name__ == "__main__":
         while i < len(lignes):
             ligne = lignes[i]
 
-            if ligne.startswith("name:"):
+            if ligne.startswith("name:"):   #si la ligne commence par "name:", on ignore cette ligne et on passe à la suivante
                 i += 1
 
             elif ligne.startswith("init:"):
-                etat_initial = ligne.split(":", 1)[1].strip()
+                etat_initial = ligne.split(":", 1)[1].strip()   #si la ligne commence par "init:", on récupère l'état initial
                 i += 1
 
-            elif ligne.startswith("accept:"):
+            elif ligne.startswith("accept:"):   #si la ligne commence par "accept:", on récupère l'état acceptant
                 etat_acceptant = ligne.split(":", 1)[1].strip()
                 i += 1
 
-            else:
-                condition = lignes[i]
+            else:   #si la ligne ne commence par rien ci-dessus, c'est que c'est une transition
+                condition = lignes[i]   #la condition de la transition est sur la ligne i, et l'instruction de la transition est sur la ligne i+1
                 instruction = lignes[i + 1]
-                morceaux_condition = [m.strip() for m in condition.split(',')]
-                morceaux_instruction = [m.strip() for m in instruction.split(',')]
+                morceaux_condition = [m.strip() for m in condition.split(',')]  #on decoupe la condition en morceaux, le premier morceau est l'état source, les suivants sont les symboles lus sur chaque ruban
+                morceaux_instruction = [m.strip() for m in instruction.split(',')]  #on decoupe l'instruction en morceaux, le premier morceau est l'état destination, les suivants sont les symboles à écrire sur chaque ruban, puis les directions à suivre pour chaque ruban
 
                 etat_source = morceaux_condition[0]
                 symboles_lus = morceaux_condition[1:]
@@ -113,20 +119,14 @@ if __name__ == "__main__":
 
                 nb_rubans = len(symboles_lus)
 
-                if len(reste_instruction) != 2 * nb_rubans:
-                    raise ValueError(
-                        "Transition invalide : le nombre de symboles ecrits et "
-                        "de directions ne correspond pas au nombre de rubans."
-                    )
-
                 symboles_ecrits = reste_instruction[:nb_rubans]
                 directions = reste_instruction[nb_rubans:]
 
                 if nb_rubans == 1:
-                    cle = (etat_source, symboles_lus[0])
-                    valeur = (etat_destination, symboles_ecrits[0], directions[0])
-                    alphabet.add(symboles_lus[0])
-                    alphabet.add(symboles_ecrits[0])
+                    cle = (etat_source, symboles_lus[0]) #si il n'y a qu'un seul ruban, il n'y a qu'un symbole dans symbole lus
+                    valeur = (etat_destination, symboles_ecrits[0], directions[0])  #pareil pour les symboles écrits et les directions
+                    alphabet.add(symboles_lus[0])   #on ajoute le symbole lu à l'alphabet de la machine
+                    alphabet.add(symboles_ecrits[0])   #on ajoute le symbole écrit à l'alphabet de la machine
                 else:
                     cle = (etat_source, tuple(symboles_lus))
                     valeur = (etat_destination, tuple(symboles_ecrits), tuple(directions))
@@ -135,15 +135,14 @@ if __name__ == "__main__":
                     for symbole in symboles_ecrits:
                         alphabet.add(symbole)
 
-                transitions[cle] = valeur
-
+                transitions[cle] = valeur   #on ajoute la transition dans le dictionnaire des transitions de la machine
                 i += 2
 
         return Machine_turing(alphabet, transitions, etat_initial, etat_acceptant, blanc, nb_rubans)
 
 
 
-    def configuration_initiale(mot, machine):
+def configuration_initiale(mot, machine):
         premier_ruban = list(mot)
         if premier_ruban == []:
             premier_ruban = [machine.blanc]
@@ -159,11 +158,11 @@ if __name__ == "__main__":
         return Configuration(rubans, positions, etat)
 
 
-    def simuler_mot(mot, machine, max_etapes=1000):
+def simuler_mot(mot, machine):
         config = configuration_initiale(mot, machine)
         nb_etapes = 0
 
-        while config.etat != machine.etat_acceptant and nb_etapes < max_etapes:
+        while config.etat != machine.etat_acceptant :
             nouvelle_config = un_pas(machine, config)
 
             if nouvelle_config is None:
@@ -176,7 +175,7 @@ if __name__ == "__main__":
         return config, accepte
 
 
-    def afficher_configuration(config):
+def afficher_configuration(config):
         print("Etat :", config.etat)
 
         for i in range(len(config.rubans)):
@@ -186,7 +185,7 @@ if __name__ == "__main__":
             print(f"          {tete}")
 
 
-    def simuler_mot_affichage(mot, machine, max_etapes=1000):
+def simuler_mot_affichage(mot, machine, max_etapes=1000):
         config = configuration_initiale(mot, machine)
         nb_etapes = 0
 
@@ -209,7 +208,7 @@ if __name__ == "__main__":
         accepte = config.etat == machine.etat_acceptant
         return config, accepte
 
-    def lire_machine2(chemin_fichier):
+def lire_machine2(chemin_fichier):
         with open(chemin_fichier, 'r', encoding='utf-8') as f:
             lignes = [ligne.strip() for ligne in f if ligne.strip()]
 
@@ -268,7 +267,7 @@ if __name__ == "__main__":
         return '|'.join(morceaux)
 
 
-    def coder_binaire(chaine):
+def coder_binaire(chaine):
         codage_binaire = {
             '0': '000',
             '1': '001',
@@ -276,7 +275,10 @@ if __name__ == "__main__":
             '_': '011',
             '<': '100',
             '-': '101',
-            '>': '110'
+            '>': '110',
+            'L': '100',
+            'S': '101',
+            'R': '110'
         }
 
         resultat = ''
@@ -286,16 +288,15 @@ if __name__ == "__main__":
         return resultat
 
 
-    def lire_machine_binaire(chemin_fichier):
+def lire_machine_binaire(chemin_fichier):
         code_machine = lire_machine2(chemin_fichier)
         return coder_binaire(code_machine)
 
 
-    def convertie_binaire(chaine_binaire):
+def convertie_binaire(chaine_binaire):
         return int(chaine_binaire, 2)
 
-
-    def decoder_machine_code(code_machine):
+def decoder_machine_code(code_machine):
         morceaux = code_machine.split('|')
 
         if len(morceaux) % 5 != 0:
@@ -319,7 +320,7 @@ if __name__ == "__main__":
         return Machine_turing(alphabet, transitions, '0', '1', blanc, 1)
 
 
-    def ruban_avec_tete(ruban, position):
+def ruban_avec_tete(ruban, position):
         ruban_marque = []
 
         for i in range(len(ruban)):
@@ -332,96 +333,94 @@ if __name__ == "__main__":
         return ' '.join(ruban_marque)
 
 
-    def afficher_rubans_universelle(ruban1, ruban2, ruban3, etape, ruban4=None):
-        print(f"Etape {etape} :")
-        print("Ruban 1 :", ruban1)
-        print("Ruban 2 :", ruban2)
-        print("Ruban 3 :", ruban3)
-        if ruban4 is not None:
-            print("Ruban 4 :", ruban4)
-        print()
+def afficher_rubans_universelle(ruban1, ruban2, ruban3, etape, ruban4=None):
+    print(f"Etape {etape} :")
+    print("Ruban 1 :", ruban1)
+    print("Ruban 2 :", ruban2)
+    print("Ruban 3 :", ruban3)
+    if ruban4 is not None:
+        print("Ruban 4 :", ruban4)
+    print()
 
 
-    def machine_universelle(entree, affichage=False):
+def machine_universelle(entree, affichage=False):
+    code_machine, mot = entree.split('#', 1)
+    machine = decoder_machine_code(code_machine)
+    config = configuration_initiale(mot, machine)
 
-        code_machine, mot = entree.split('#', 1)
-        machine = decoder_machine_code(code_machine)
-        config = configuration_initiale(mot, machine)
+    ruban1 = entree
+    ruban2 = code_machine + '#' + config.etat
+    ruban3 = ruban_avec_tete(config.rubans[0], config.positions[0])
+    nb_etapes = 0
 
-        ruban1 = entree
-        ruban2 = code_machine + '#' + config.etat
-        ruban3 = ruban_avec_tete(config.rubans[0], config.positions[0])
-        nb_etapes = 0
+    if affichage:
+        afficher_rubans_universelle(ruban1, ruban2, ruban3, nb_etapes)
 
-        if affichage:
-            afficher_rubans_universelle(ruban1, ruban2, ruban3, nb_etapes)
+    while config.etat != machine.etat_acceptant:
+        nouvelle_config = un_pas(machine, config)
 
-        while config.etat != machine.etat_acceptant:
-            nouvelle_config = un_pas(machine, config)
-
-            if nouvelle_config is None:
-                ruban2 = code_machine + '#blocage'
-                ruban3 = ruban_avec_tete(config.rubans[0], config.positions[0])
-
-                if affichage:
-                    afficher_rubans_universelle(ruban1, ruban2, ruban3, nb_etapes)
-
-                return ruban1, ruban2, ruban3, False
-
-            config = nouvelle_config
-            nb_etapes += 1
-
-            ruban2 = code_machine + '#' + config.etat
+        if nouvelle_config is None:
+            ruban2 = code_machine + '#blocage'
             ruban3 = ruban_avec_tete(config.rubans[0], config.positions[0])
 
             if affichage:
                 afficher_rubans_universelle(ruban1, ruban2, ruban3, nb_etapes)
 
-        accepte = config.etat == machine.etat_acceptant
-        return ruban1, ruban2, ruban3, accepte
+            return ruban1, ruban2, ruban3, False
 
+        config = nouvelle_config
+        nb_etapes += 1
 
-    def machine_universelle_n_etapes(entree, affichage=False):
-        parties = entree.split('#')
-
-        code_machine, mot, etape = parties
-        machine = decoder_machine_code(code_machine)
-        config = configuration_initiale(mot, machine)
-        compteur_restant = int(etape)
-
-        ruban1 = entree
         ruban2 = code_machine + '#' + config.etat
         ruban3 = ruban_avec_tete(config.rubans[0], config.positions[0])
-        ruban4 = '1' * compteur_restant + '_'
-        nb_etapes = 0
 
         if affichage:
-            afficher_rubans_universelle(ruban1, ruban2, ruban3, nb_etapes, ruban4)
+            afficher_rubans_universelle(ruban1, ruban2, ruban3, nb_etapes)
 
-        while config.etat != machine.etat_acceptant and compteur_restant > 0:
-            nouvelle_config = un_pas(machine, config)
+    accepte = config.etat == machine.etat_acceptant
+    return ruban1, ruban2, ruban3, accepte
 
-            if nouvelle_config is None:
-                ruban2 = code_machine + '#blocage'
-                ruban3 = ruban_avec_tete(config.rubans[0], config.positions[0])
-                ruban4 = '1' * compteur_restant + '_'
 
-                if affichage:
-                    afficher_rubans_universelle(ruban1, ruban2, ruban3, nb_etapes, ruban4)
+def machine_universelle_n_etapes(entree, affichage=False):
+    parties = entree.split('#')
 
-                return ruban1, ruban2, ruban3, ruban4, False
+    code_machine, mot, etape = parties
+    machine = decoder_machine_code(code_machine)
+    config = configuration_initiale(mot, machine)
+    compteur_restant = int(etape)
 
-            config = nouvelle_config
-            nb_etapes += 1
-            compteur_restant -= 1
+    ruban1 = entree
+    ruban2 = code_machine + '#' + config.etat
+    ruban3 = ruban_avec_tete(config.rubans[0], config.positions[0])
+    ruban4 = '1' * compteur_restant + '_'
+    nb_etapes = 0
 
-            ruban2 = code_machine + '#' + config.etat
+    if affichage:
+        afficher_rubans_universelle(ruban1, ruban2, ruban3, nb_etapes, ruban4)
+
+    while config.etat != machine.etat_acceptant and compteur_restant > 0:
+        nouvelle_config = un_pas(machine, config)
+
+        if nouvelle_config is None:
+            ruban2 = code_machine + '#blocage'
             ruban3 = ruban_avec_tete(config.rubans[0], config.positions[0])
             ruban4 = '1' * compteur_restant + '_'
 
             if affichage:
                 afficher_rubans_universelle(ruban1, ruban2, ruban3, nb_etapes, ruban4)
 
-        accepte = config.etat == machine.etat_acceptant
-        return ruban1, ruban2, ruban3, ruban4, accepte
+            return ruban1, ruban2, ruban3, ruban4, False
 
+        config = nouvelle_config
+        nb_etapes += 1
+        compteur_restant -= 1
+
+        ruban2 = code_machine + '#' + config.etat
+        ruban3 = ruban_avec_tete(config.rubans[0], config.positions[0])
+        ruban4 = '1' * compteur_restant + '_'
+
+        if affichage:
+            afficher_rubans_universelle(ruban1, ruban2, ruban3, nb_etapes, ruban4)
+
+    accepte = config.etat == machine.etat_acceptant
+    return ruban1, ruban2, ruban3, ruban4, accepte
